9700c88411aae0f531ed455cd62fddb321add329
Promiles0 <apaersod@gmail.com>
2026-06-27 19:51:30 +0200

    fav icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -100,13 +100,13 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "icon",
         type: "image/png",
         sizes: "32x32",
-        href: "/assets/favicon-32x32.png",
+        href: "/assets/favicon.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "16x16",
-        href: "/assets/favicon-16x16.png",
+        href: "/assets/favicon.png",
       },
       {
         rel: "icon",
